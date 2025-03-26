f8516ae97da3bd5ae4bdbc194e9f4c4424ad8824
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-03-26 17:54:03 -0400

    add Status enum and Task construction method

--- src/task.rs
+++ src/task.rs
@@ -1,6 +1,22 @@
+pub enum Status {
+    NotStarted,
+    InProgress,
+    Completed,
+}
+
 pub struct Task {
     id: u32,
     name: String,
-    Description: String,
-    completed: bool,
+    description: String,
+    completed: Status,
+}
+
+impl Task {
+    fn new(&self, name: String, description: String) -> Self {
+        Task {
+            name,
+            description,
+            completed: Status::NotStarted,
+        }
+    }
 }
