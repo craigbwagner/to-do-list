19ccd3f81e12b7b0e817e2f70bda16e510a61913
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-03-26 19:03:25 -0400

    create instance of Task, import task module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod task;
+
+use crate::task::{Status, Task};
+
 fn main() {
-    println!("Hello, world!");
+    let mut sample_task = Task {
+        id: 1,
+        name: "Do dishes".to_string(),
+        description: "Obvious description".to_string(),
+        status: Status::NotStarted,
+    };
+
+    print!("{:#?}", sample_task)
 }
