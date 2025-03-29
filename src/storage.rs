8d4cb10537cd97a92c774878a22bda45a732ce09
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-03-29 17:05:38 -0400

    fix task parameter Result taking String and Error for store_task, now only takes a result with type String

--- src/storage.rs
+++ src/storage.rs
@@ -5,12 +5,13 @@ fn create_json(task: Task) -> Result<()> {
     // serialize to JSON string
     let task_json = serde_json::to_string(&task)?;
 
+    // ? should I do a match pattern here on task_json?
     store_task(Ok(task_json));
 
     Ok(())
 }
 
-fn store_task(task: Result<String, Error>) -> () {
+fn store_task(task: Result<String>) -> () {
     match task {
         Ok(task_string) => {}
         Err(e) => {}
