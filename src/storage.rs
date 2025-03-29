acef0346adf2244549784228aa5b367c5396be5c
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-03-29 17:01:38 -0400

    add store_task function for writing tasks to json file

--- src/storage.rs
+++ src/storage.rs
@@ -1,8 +1,18 @@
 use crate::task::Task;
-use serde_json::Result;
+use serde_json::{Error, Result};
 
 fn create_json(task: Task) -> Result<()> {
-    let task_json = serde_json::to_string(&task);
+    // serialize to JSON string
+    let task_json = serde_json::to_string(&task)?;
+
+    store_task(Ok(task_json));
 
     Ok(())
+}
+
+fn store_task(task: Result<String, Error>) -> () {
+    match task {
+        Ok(task_string) => {}
+        Err(e) => {}
+    }
 }
