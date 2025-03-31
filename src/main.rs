9805c8d33ffcf0136d3eee3c6b3a79da877f8530
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-03-30 20:18:24 -0400

    use read_tasks to pull in existing tasks from storage

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 mod storage;
 mod task;
 
-use storage::create_json;
+use storage::{read_tasks, store_task};
 
 use crate::task::Task;
 
 fn main() {
+    let existing_tasks = read_tasks();
+
     let name = "Dishes".to_string();
     let description = "General Description".to_string();
     let sample_task = Task::new(name, description);
 
-    create_json(sample_task).expect("Failed to create task");
+    for task in existing_tasks {
+        dbg!(task);
+    }
 }
