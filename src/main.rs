b7868db2bfe6a457ce40a0cbf1a6390f6ab6304d
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-03-30 21:07:17 -0400

    remove use for storage mod and its functions to be more idiomatic with the rust language

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 mod storage;
 mod task;
 
-use storage::{read_tasks, store_task};
-
 use crate::task::Task;
 
 fn main() {
-    let existing_tasks = read_tasks();
+    let existing_tasks = storage::read_tasks();
 
     let name = "Dishes".to_string();
     let description = "General Description".to_string();
