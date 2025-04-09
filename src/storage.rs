947b78033168d6a038f681681a66b3ff86799f0a
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-04-08 21:56:20 -0400

    update error reporting in store_task function

--- src/storage.rs
+++ src/storage.rs
@@ -1,16 +1,15 @@
 use crate::task::Task;
-use serde_json::Result;
 
 use std::fs::File;
-use std::io::{self, BufRead, Error, Write};
+use std::io::{self, BufRead, Error, ErrorKind, Write};
 
-pub fn store_task(task: Task) -> Result<()> {
+pub fn store_task(task: Task) -> Result<(), Error> {
     // serialize to JSON string
-    let task_json = serde_json::to_string(&task)?;
-    let mut file = File::create("tasks.json").expect("Failed to create file.");
+    let task_json =
+        serde_json::to_string(&task).map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
+    let mut file = File::create("tasks.json")?;
 
-    file.write(task_json.as_bytes())
-        .expect("Unable to write file");
+    file.write_all(task_json.as_bytes())?;
 
     Ok(())
 }
@@ -28,6 +27,6 @@ pub fn read_tasks() -> Vec<Task> {
 }
 
 fn read_lines() -> std::result::Result<io::Lines<io::BufReader<File>>, Error> {
-    let file = File::open("../tasks.json").expect("Failed to open file.");
+    let file = File::open("./tasks.json").expect("Failed to open file.");
     Ok(io::BufReader::new(file).lines())
 }
